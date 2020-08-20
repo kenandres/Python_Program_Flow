f090ba32019ca6671eda156ff6d9786d2122a9d0
Kenneth Andres <kyogenonline@gmail.com>
2020-08-20 14:34:10 -0400

    Add files via upload

---
+++ mutable.py
@@ -0,0 +1,10 @@
+shopping_list = ["milk",
+                 "sugar",
+                 "spam",
+                 "eggs",
+                 "bread",
+                 "rice"]
+
+another_list = shopping_list
+print(id(shopping_list))
+print(id(another_list))
