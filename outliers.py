5453b1f29a407ac6bd309562783b3f0adaf37a49
Kenneth Andres <kyogenonline@gmail.com>
2020-09-04 14:26:56 -0400

    Outliers

--- outliers.py
+++ outliers.py
@@ -9,8 +9,26 @@ data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
 min_valid = 100
 max_valid = 200
 
+stop = 0
 for index, value in enumerate(data):
-    if (value < min_valid) or (value > min_valid):
-        del data[index]
+    if value >= min_valid:
+        stop = index
+        break
 
+print(stop)  # for debugging
+del data[:stop]
+print(data)
+
+# process the high values in the list
+start = 0
+for index in range(len(data) - 1, -1, -1):  # range(start, stop, step)
+    if data[index] <= max_valid:
+        # We have the index of the last item to keep.
+        # Set 'start' to the position of the first
+        # item to delete, which is 1 after 'index'.
+        start = index + 1
+        break
+
+print(start)  # for debugging
+del data[start:]
 print(data)
